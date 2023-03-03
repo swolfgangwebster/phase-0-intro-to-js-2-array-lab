95d31dfa19ea7f10a94b74543e6bdce4e96fc810
Shawn Webster <swolfgangwebster@gmail.com>
2023-03-03 16:35:03 -0600

    cats

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(n){
+    cats.push(n);
+}
+
+function destructivelyPrependCat(n){
+    cats.unshift(n);
+}
+
+function destructivelyRemoveFirstCat() {cats.shift()}
+function destructivelyRemoveLastCat() {cats.pop()}
+
+function appendCat(n) {
+    return [...cats,n]
+}
+
+function prependCat(n) {
+    return [n, ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length-1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1, cats.length)
+}
